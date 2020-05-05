2ca1a38be0959fe9f4f5015f01ee4da3b7140e2a
Miso <peckykom@gmail.com>
2020-05-05 23:07:20 +0200

    Spring Initializr

---
+++ src/main/java/com/coctails/MyCoctails/MyCoctailsApplication.java
@@ -0,0 +1,13 @@
+package com.coctails.MyCoctails;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyCoctailsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyCoctailsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/coctails/MyCoctails/MyCoctailsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coctails.MyCoctails;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyCoctailsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
